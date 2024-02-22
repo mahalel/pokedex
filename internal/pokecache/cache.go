cf94d24aabdc7d38b3a83ec313b74355c49c523d
Andrei Mahalean <andrei@maha.nz>
2024-02-22 22:49:26 +1300

    some progress on the pokecache

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -5,12 +5,15 @@ import (
 	"time"
 )
 
-type Cache struct {
-	entry map[string]cacheEntry
-	mu    *sync.Mutex
-}
-
+// cacheEntry represents an entry in the cache.
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+// Cache is a struct that holds the cache data and provides methods for safe access.
+type Cache struct {
+	data     map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
 }
--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,7 +1,42 @@
 package pokecache
 
-import "time"
+import (
+	"time"
+)
 
-func NewCache(interval time.Duration) Cache {
-	return Cache{}
+// This function will create a new cache, it will return a pointer to the cache
+// and create a new map with the zero value for it's type
+func NewCache(interval time.Duration) *Cache {
+	return &Cache{
+		data: make(map[string]cacheEntry),
+	}
+}
+
+// This function will add a key to the cache
+// It will use a mutex to lock the cache while we are adding a new key
+// It will defer the unlock
+func (cache *Cache) Add(key string, value []byte) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       value,
+	}
+}
+
+// This function will return a key and its value from the cache if it exists
+// It will use a mutex to lock the cache while we are getting a key
+// It will defer the unlock
+func (cache *Cache) Get(key string) ([]byte, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	// Check if the element exists
+	elem, ok := cache.data[key]
+	return elem.val, ok
+
+}
+
+func (cache *Cache) reapLoop() {
+	return
 }
