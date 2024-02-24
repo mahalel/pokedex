578231457f77dfe0c1bf28c44b609a815ede7586
Andrei Mahalean <andrei@maha.nz>
2024-02-25 00:50:14 +1300

    test gpg unlock from windows

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -5,13 +5,13 @@ import (
 	"time"
 )
 
-// cacheEntry represents an entry in the cache.
+// cacheEntry - represents an entry in the cache.
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
 }
 
-// Cache is a struct that holds the cache data and provides methods for safe access.
+// Cache - is a struct that holds the cache data and provides methods for safe access.
 type Cache struct {
 	data     map[string]cacheEntry
 	mu       sync.Mutex
