7755e5f80c352d9721f5b0523a3ba409a12607cd
Andrei Mahalean <andrei@maha.nz>
2024-04-08 21:42:19 +1200

    done

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -33,7 +33,7 @@ func (c *Cache) Add(key string, value []byte) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
 	c.cache[key] = cacheEntry{
-		createdAt: time.Now(),
+		createdAt: time.Now().UTC(),
 		val:       value,
 	}
 }
